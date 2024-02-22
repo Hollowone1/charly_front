d387ad9238e4fad7f20b19df7d327c0b194f4323
Regis <vavasseu2u@etu.univ-lorraine.fr>
2024-02-22 19:46:22 +0100

    fix: changement du nom de la méthode pour fetch

--- Charly_front/src/js/loader.js
+++ Charly_front/src/js/loader.js
@@ -1,6 +1,6 @@
 class loader {
 
-    fetch_festival_api(url, options){
+    fetch_api(url, options){
         return fetch('https://backend.crazycharlyday.kiwigdc.fr/' + url, options)
             .then(response => {
                 if (response.ok) return response.json();
