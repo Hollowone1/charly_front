b2f7938adf520c49803cb19786c550655c088316
BJulOs <julesbresson88600@gmail.com>
2024-02-22 14:28:38 +0100

    store Pinia

--- Charly_front/src/App.vue
+++ Charly_front/src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import {RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 import BasDePage from './components/Footer.vue';
 import Navigation from './components/Header.vue';
 
@@ -14,7 +14,7 @@ export default {
 </script>
 
 <template>
-    <Navigation />
-    <RouterView />
-    <BasDePage/>
-  </template>
+  <Navigation/>
+  <RouterView/>
+  <BasDePage/>
+</template>
