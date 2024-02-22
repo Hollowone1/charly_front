7ad1fa0df4add3216bc2d445b9e84eb2dbcea458
BJulOs <julesbresson88600@gmail.com>
2024-02-22 14:52:26 +0100

    add bootstrap to main

--- Charly_front/src/main.js
+++ Charly_front/src/main.js
@@ -1,14 +1,16 @@
-
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'vue3-toastify/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
+import toast from './plugins/toast'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(toast)
 
 app.mount('#app')
