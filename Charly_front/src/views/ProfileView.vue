1e92c2d88829415cf4c45035cf1b51bd1375b167
Regis <vavasseu2u@etu.univ-lorraine.fr>
2024-02-22 18:34:16 +0100

    fix: modif de la page profil réalisé par antoine

--- Charly_front/src/views/ProfileView.vue
+++ Charly_front/src/views/ProfileView.vue
@@ -3,7 +3,7 @@
         <div class="profil">
             <h2>Mon profil</h2>
             <form>
-                <img src="@/assets/profil.jpg" alt="photo de profil">
+                <img src="@/assets/profil.png" alt="photo de profil">
                 <label for="name">Votre nom</label>
                 <input type="text">
                 <label for="surname">Votre prénom</label>
@@ -15,29 +15,63 @@
         </div>
         <section class="demands">
             <h2>Mes demandes</h2>
-            <div class="card rounded">
-                <div class="container-fluid">
-                    <div class="row border-top-radius">
-                        <div class="col-md-6">
-                            <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+            <div class="cards">
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
                         </div>
-                        <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
-                            <h5 class="card-title">Soupe à l'oignon</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
+                        </div>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <div class="card-status">
+                                    <h5 class="status-btn">Status:
+                                        <div class="state-status">Acceptée</div>
+                                    </h5>
+                                </div>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                </div>
+
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row border-bottom-radius">
-                        <div class="col-md-12">
-                            <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
-                            <div class="card-status"> <h5 class="status-btn">Status:
-                            <div class="state-status">Acceptée</div>
-                            </h5></div>
-                            <p class="card-date">Date: 12/04/2024</p>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
+                        </div>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <div class="card-status">
+                                    <h5 class="status-btn">Status:
+                                        <div class="state-status">Acceptée</div>
+                                    </h5>
+                                </div>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -45,29 +79,107 @@
         </section>
         <section class="ateliers">
             <h2>Mes ateliers passés</h2>
-            <div class="card rounded">
-                <div class="container-fluid">
-                    <div class="row border-top-radius">
-                        <div class="col-md-6">
-                            <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+            <div class="cards">
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
                         </div>
-                        <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
-                            <h5 class="card-title">Soupe à l'oignon</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
-                        </div>
-                    </div>
-                    <div class="row border-bottom-radius">
-                        <div class="col-md-12">
-                            <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
-                            <p class="card-date">Date: 12/04/2024</p>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
+                        </div>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
+                        </div>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card rounded">
+                    <div class="container-fluid">
+                        <div class="row border-top-radius">
+                            <div class="col-md-6">
+                                <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                            </div>
+                            <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="card-title">Soupe à l'oignon</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">recette orientale</h6>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                            </div>
+                        </div>
+                        <div class="row border-bottom-radius">
+                            <div class="col-md-12">
+                                <h3 class="card-title mt-3">Recettes de la soupe à l'oignon à l'orientale</h3>
+                                <p class="card-date">Date: 12/04/2024</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     </main>
@@ -82,75 +194,79 @@
 @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
 
 
-
 .profil {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 }
 
 .profil h1 {
-    margin-bottom: 20px;
-    font-family: 'Comfortaa',sans-serif;
-    font-weight: 400;
+  margin-bottom: 20px;
+  font-family: 'Comfortaa', sans-serif;
+  font-weight: 400;
 }
 
 .profil form {
-    display: grid;
-    gap: 20px;
-    width: 100%;
-    max-width: 300px;
+  display: grid;
+  gap: 20px;
+  width: 100%;
+  max-width: 300px;
 }
 
 .profil form label {
-    display: block;
-    margin-bottom: 5px;
+  display: block;
+  margin-bottom: 5px;
 }
 
 .profil form input {
-    width: 100%;
+  width: 100%;
 }
+
 .profil form img {
-    align-self: center;
-    justify-self: center;
+  align-self: center;
+  justify-self: center;
 }
 
-form img{
-    height: 200px;
-    width: 200px;
+form img {
+  height: 200px;
+  width: 200px;
 }
 
-label{
-    text-align: center;
-    font-family: 'Comfortaa',sans-serif;
-    font-weight: 300;
+label {
+  text-align: center;
+  font-family: 'Comfortaa', sans-serif;
+  font-weight: 300;
 }
 
-input{
-    max-width: 400px;
-    height: 45px;
-    border-radius: 5%;
+input {
+  max-width: 400px;
+  height: 45px;
+  border-radius: 5%;
 }
-form button{
-    max-width: 200px;
-    align-self: center;
-    justify-self: center;
-    background-color: #A43481;
-    border-radius: 10%;
-    height: 50px;
-    color: #fff;
-    font-family: 'Comfortaa',sans-serif;
-    font-weight: 300;
-    margin-bottom: 30px;
+
+form button {
+  max-width: 200px;
+  align-self: center;
+  justify-self: center;
+  background-color: #A43481;
+  border-radius: 20px;
+  height: 50px;
+  color: #fff;
+  font-family: 'Comfortaa', sans-serif;
+  font-weight: 300;
+  margin-bottom: 30px;
 }
-.demands{
-    background-color: #37A962;
-    height: 800px;
+
+.demands {
+  background-color: #37A962;
+  height: 600px;
 }
-.ateliers{
-    height:800px
+
+.ateliers {
+  height: 600px;
 }
+
 .card {
   width: 100%;
   max-width: 300px;
@@ -161,47 +277,55 @@ form button{
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
+
   .container-fluid {
     padding: 0;
+
     .row {
       margin: 0;
       background-color: white;
+
       &:first-child {
         display: flex;
         align-items: bottom;
-        justify-content:center;
+        justify-content: center;
+
         img {
-            width: 50%;
-            height: auto;
-            object-fit: cover;
-            border-radius: 100%;
+          width: 50%;
+          height: auto;
+          object-fit: cover;
+          border-radius: 100%;
         }
+
         .col-md-6 {
           border-top-right-radius: 10px;
           border-top-left-radius: 10px;
           display: flex;
           flex-direction: column;
-          justify-content:center;
-          align-items:flex-start;
+          justify-content: center;
+          align-items: flex-start;
           padding: 0;
           max-width: 130px;
+
           .card-title {
             margin-bottom: 5px;
             color: #000;
             text-align: start;
-            font-family: 'Comfortaa',sans-serif;
+            font-family: 'Comfortaa', sans-serif;
             font-weight: 500;
           }
+
           .card-subtitle {
             margin-bottom: 5px;
             color: #000;
             text-align: start;
-            font-family: 'Comfortaa',sans-serif;
+            font-family: 'Comfortaa', sans-serif;
             font-weight: 300;
-            
+
           }
         }
       }
+
       &:nth-child(2) {
         img {
           width: 100%;
@@ -209,56 +333,71 @@ form button{
           object-fit: contain;
         }
       }
+
       &:last-child {
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 0;
+
         .card-title {
           margin-bottom: 10px;
           color: #000;
-          font-family: 'Comfortaa',sans-serif;
+          font-family: 'Comfortaa', sans-serif;
           font-weight: 600;
           text-align: center;
         }
+
         .card-status {
           margin-bottom: 20px;
           color: #000;
+
           .status-btn {
             display: flex;
             justify-content: center;
             align-items: center;
+
             .state-status {
               margin-left: 5px;
               padding: 5px 10px;
               border-radius: 5px;
-                background-color: #37A962;
+              background-color: #37A962;
             }
           }
         }
+
         .card-date {
           text-align: center;
           color: #000;
-          font-family: 'Comfortaa',sans-serif;
+          font-family: 'Comfortaa', sans-serif;
           font-weight: 300;
         }
       }
     }
   }
 }
+
 .border-top-radius {
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
 }
+
 .border-bottom-radius {
   border-bottom-right-radius: 10px;
   border-bottom-left-radius: 10px;
 }
 
-h2{
-    text-align: center;
-    padding: 10px;
-    margin-top: 20px;
-    margin-bottom: 20px;
+h2 {
+  text-align: center;
+  padding: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.cards {
+  display: flex;
+  justify-content: space-around;
+    flex-wrap: wrap;
+
 }
 </style>
