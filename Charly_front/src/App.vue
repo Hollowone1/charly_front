d74eb2e0e05bf4d971d482c74f92aa5ab251444e
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-22 13:25:41 +0100

    app vue for routing

---
+++ Charly_front/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import {RouterView } from 'vue-router'
+import BasDePage from './components/Footer.vue';
+import Navigation from './components/Header.vue';
+
+export default {
+  components: {
+    RouterView,
+    BasDePage,
+    Navigation
+
+  },
+}
+</script>
+
+<template>
+    <Navigation />
+    <RouterView />
+    <BasDePage/>
+  </template>
