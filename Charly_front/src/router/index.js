9b39719e5e0d51ad86070ecc3e791692bd8c7e3e
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-22 13:19:49 +0100

    router done

--- Charly_front/src/router/index.js
+++ Charly_front/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileViewVue from '@/views/ProfileView.vue'
+import ListViewVue from '@/views/ListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'accueil',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/profil',
+      name: 'profil',
+      component: ProfileViewVue
+    },
+    {
+      path: '/list',
+      name: 'liste-des-recettes',
+      component: ListViewVue
     }
   ]
 })
