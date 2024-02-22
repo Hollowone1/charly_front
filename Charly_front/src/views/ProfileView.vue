b782cb377189acc30f4f6501302025acb2573be4
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-22 14:50:36 +0100

    profile view first change

--- Charly_front/src/views/ProfileView.vue
+++ Charly_front/src/views/ProfileView.vue
@@ -9,13 +9,43 @@
             <input type="text">
             <label for="email">Votre email</label>
             <input type="text">
+            <button type="submit">Modifier</button>
         </form>
+        <section class="demands">
+            <div class="card">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="@/assets/ragout.jpg" alt="image de la recette" class="img-fluid">
+                        </div>
+                        <div class="col-md-6 d-flex flex-column justify-content-center align-items-start">
+                            <h5 class="card-title">Title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Subtitle</h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="@/assets/cuisine.png" alt="image du cours" class="img-fluid w-100">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="card-title mt-3">Main Title</h3>
+                            <h5 class="card-status">Status: <span class="status-btn">
+                            <button class="btn btn-success btn-sm">Accepted</button>
+                            <button class="btn btn-warning btn-sm">Pending</button>
+                            <button class="btn btn-danger btn-sm">Rejected</button>
+                            </span></h5>
+                            <p class="card-date">Date: 12/04/2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ateliers">
+            
+        </section>
     </main>
-
-
-
-
-
 
 </template>
 
@@ -24,5 +54,40 @@
 </script>
 
 <style scoped lang="scss">
+.card {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 20px;
+    margin: 20px;
+}
 
+.card-title {
+    margin-bottom: 10px;
+}
+
+.card-subtitle {
+    margin-bottom: 20px;
+}
+
+.card-status {
+    margin-bottom: 10px;
+}
+
+.card-date {
+    margin-top: 10px;
+}
+
+.status-btn {
+    display: inline-flex;
+    gap: 5px;
+}
+
+.img-fluid {
+    max-width: 100%;
+    height: auto;
+}
+
+.w-100 {
+    width: 100%;
+}
 </style>
